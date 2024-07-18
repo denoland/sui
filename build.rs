01807df3ea9a90cf6bd47045fbaa8507a4e4f319
Divy Srivastava <dj.srivastava23@gmail.com>
2024-07-18 16:25:45 +0530

    x

--- build.rs
+++ build.rs
@@ -10,8 +10,8 @@ fn main() {
         config
             .define("BUILD_SHARED_LIBS", "OFF")
             .define("LIEF_USE_CRT_RELEASE", "MTd")
-            .define("CMAKE_BUILD_TYPE", "Release")
-            .static_crt(true);
+            .static_crt(true)
+            .build();
         let dst = config.build();
 
         println!(
