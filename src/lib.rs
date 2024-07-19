0b3b26872ffe5b1ebcc89ce952ae9ac9e0070081
Divy Srivastava <dj.srivastava23@gmail.com>
2024-07-19 19:47:17 +0530

    Hmmm

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,13 @@ pub fn inject_pe(
                 return Err("icon table is not a table".to_string());
             }
         };
-        let inner_table = rc_table.get(*rc_table.entries().first().unwrap());
+        if rc_table.get(ResourceEntryName::from_string(name)).is_none() {
+            rc_table.insert(
+                ResourceEntryName::from_string(name),
+                ResourceEntry::Table(ResourceTable::default()),
+            );
+        }
+        let inner_table = rc_table.get(ResourceEntryName::from_string(name));
         let mut inner_table = match inner_table {
             Some(ResourceEntry::Table(t)) => t.clone(),
             Some(_) => {
