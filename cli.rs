f80b983c45986f3fb1898c1e5ed434bf3cd59265
Divy Srivastava <dj.srivastava23@gmail.com>
2024-07-21 10:48:16 +0530

    Macho rewrite

--- cli.rs
+++ cli.rs
@@ -1,8 +1,10 @@
 use libsui::find_section;
+use libsui::Macho;
+use libsui::PortableExecutable;
 
-const HELP: &str = r#"Usage: sui <exe> <segment> <data_file> <output>"#;
+const HELP: &str = r#"Usage: sui <exe> <data_file> <output>"#;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 5 {
         if let Some(section) = find_section("__SUI") {
@@ -12,11 +14,22 @@ fn main() {
         std::process::exit(1);
     }
 
-    let exe = std::fs::read(&args[1]).unwrap();
-    let data = std::fs::read(&args[3]).unwrap();
+    let exe = std::fs::read(&args[1])?;
+    let data = std::fs::read(&args[3])?;
 
+    let mut out = std::fs::File::create(&args[4])?;
     // libsui::inject_macho(&exe, &args[2], &args[2], &data, &args[4]);
     // libsui::inject_pe(&exe, &args[2], &data, &args[4]).unwrap();
 
-    libsui::inject_elf(&exe, &args[2], &data, &args[4]).unwrap();
+    // PortableExecutable::from(&exe)?
+    //    .write_resource("_SUI", data)?
+    //    .build(&mut out)?;
+
+    Macho::from(exe)
+        .write_section("__SUI", data)?
+        .build(&mut out)?;
+
+    //    Elf::new(&exe)
+    //        .append(&data);
+    Ok(())
 }
--- lib.rs
+++ lib.rs
@@ -22,6 +22,11 @@
  * THE SOFTWARE.
  */
 
+use editpe::{
+    constants::{LANGUAGE_ID_EN_US, RT_RCDATA},
+    ResourceEntry, ResourceEntryName, ResourceTable,
+};
+
 #[cfg(target_os = "linux")]
 pub use elf::find_section;
 #[cfg(target_os = "macos")]
@@ -29,73 +34,109 @@ pub use macho::find_section;
 #[cfg(target_os = "windows")]
 pub use pe::find_section;
 
-// pe
+#[derive(Debug)]
+pub enum Error {
+    InvalidObject(&'static str),
+    InternalError,
+    IoError(std::io::Error),
+}
 
-use editpe::constants::LANGUAGE_ID_EN_US;
-use editpe::constants::RT_RCDATA;
-use editpe::Image;
-use editpe::ResourceEntry;
-use editpe::ResourceEntryName;
-use editpe::ResourceTable;
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::InvalidObject(msg) => write!(f, "Invalid object: {}", msg),
+            Error::InternalError => write!(f, "Internal error"),
+            Error::IoError(err) => write!(f, "I/O error: {}", err),
+        }
+    }
+}
 
-pub fn inject_pe(pe: &[u8], name: &str, sectdata: &[u8], outfile: &str) -> Result<(), String> {
-    let mut image = Image::parse(pe).unwrap();
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::IoError(err)
+    }
+}
 
-    let mut resources = image.resource_directory().cloned().unwrap_or_default();
-    let root = resources.root_mut();
-    if root.get(ResourceEntryName::ID(RT_RCDATA as u32)).is_none() {
-        root.insert(
-            ResourceEntryName::ID(RT_RCDATA as u32),
+impl std::error::Error for Error {}
+
+/// A portable executable (PE)
+///
+/// Build a new PE from existing PE and write auxillary data as
+/// a resource in the .rsrc section.
+pub struct PortableExecutable<'a> {
+    image: editpe::Image<'a>,
+}
+
+impl<'a> PortableExecutable<'a> {
+    /// Parse from a PE file
+    pub fn from(data: &'a [u8]) -> Result<Self, Error> {
+        Ok(Self {
+            image: editpe::Image::parse(data)
+                .map_err(|_| Error::InvalidObject("Failed to parse PE"))?,
+        })
+    }
+
+    /// Write a resource to the PE file
+    pub fn write_resource(mut self, name: &str, sectdata: Vec<u8>) -> Result<Self, Error> {
+        let mut resources = self.image.resource_directory().cloned().unwrap_or_default();
+        let root = resources.root_mut();
+        if root.get(ResourceEntryName::ID(RT_RCDATA as u32)).is_none() {
+            root.insert(
+                ResourceEntryName::ID(RT_RCDATA as u32),
+                ResourceEntry::Table(ResourceTable::default()),
+            );
+        }
+        let rc_table = match root
+            .get_mut(ResourceEntryName::ID(RT_RCDATA as u32))
+            .unwrap()
+        {
+            ResourceEntry::Table(table) => table,
+            ResourceEntry::Data(_) => {
+                return Err(Error::InvalidObject("RCDATA is not a table"));
+            }
+        };
+        rc_table.insert(
+            editpe::ResourceEntryName::from_string(name),
             ResourceEntry::Table(ResourceTable::default()),
         );
+
+        let rc_table = match rc_table
+            .get_mut(editpe::ResourceEntryName::from_string(name))
+            .unwrap()
+        {
+            ResourceEntry::Table(table) => table,
+            ResourceEntry::Data(_) => {
+                return Err(Error::InvalidObject("Resource entry is not a table"));
+            }
+        };
+        let mut entry = editpe::ResourceData::default();
+        entry.set_data(sectdata);
+
+        rc_table.insert(
+            ResourceEntryName::ID(LANGUAGE_ID_EN_US as u32),
+            ResourceEntry::Data(entry),
+        );
+
+        self.image
+            .set_resource_directory(resources)
+            .map_err(|_| Error::InternalError)?;
+        Ok(self)
     }
-    let rc_table = match root
-        .get_mut(ResourceEntryName::ID(RT_RCDATA as u32))
-        .unwrap()
-    {
-        ResourceEntry::Table(table) => table,
-        ResourceEntry::Data(_) => {
-            return Err("icon table is not a table".to_string());
-        }
-    };
-    rc_table.insert(
-        editpe::ResourceEntryName::from_string(name),
-        ResourceEntry::Table(ResourceTable::default()),
-    );
 
-    let rc_table = match rc_table
-        .get_mut(editpe::ResourceEntryName::from_string(name))
-        .unwrap()
-    {
-        ResourceEntry::Table(table) => table,
-        ResourceEntry::Data(_) => {
-            return Err("icon table is not a table".to_string());
-        }
-    };
-    let mut entry = editpe::ResourceData::default();
-    entry.set_data(sectdata.to_vec());
-
-    rc_table.insert(
-        ResourceEntryName::ID(LANGUAGE_ID_EN_US as u32),
-        ResourceEntry::Data(entry),
-    );
-
-    image.set_resource_directory(resources).unwrap();
-
-    let target = image.data();
-    std::fs::write(outfile, target).unwrap();
-
-    Ok(())
+    /// Build and write the modified PE file
+    pub fn build<W: std::io::Write>(&self, writer: &mut W) -> Result<(), Error> {
+        let data = self.image.data();
+        writer.write_all(data)?;
+        Ok(())
+    }
 }
 
 #[cfg(target_os = "windows")]
 mod pe {
     use std::ffi::CString;
-
-    use windows_sys::Win32::System::LibraryLoader::FindResourceA;
-    use windows_sys::Win32::System::LibraryLoader::LoadResource;
-    use windows_sys::Win32::System::LibraryLoader::LockResource;
-    use windows_sys::Win32::System::LibraryLoader::SizeofResource;
+    use windows_sys::Win32::System::LibraryLoader::{
+        FindResourceA, LoadResource, LockResource, SizeofResource,
+    };
 
     pub fn find_section(section_name: &str) -> Option<&[u8]> {
         let Ok(section_name) = CString::new(section_name.to_uppercase()) else {
@@ -128,7 +169,6 @@ mod pe {
 }
 
 // mach-o
-
 #[cfg(target_os = "macos")]
 mod macho {
     use std::ffi::CString;
@@ -174,8 +214,10 @@ mod macho {
     }
 }
 
+use zerocopy::{AsBytes, FromBytes, FromZeroes};
+
 #[repr(C)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, FromBytes, FromZeroes, AsBytes)]
 pub struct SegmentCommand64 {
     pub cmd: u32,
     pub cmdsize: u32,
@@ -190,6 +232,7 @@ pub struct SegmentCommand64 {
     pub flags: u32,
 }
 
+#[derive(FromBytes, FromZeroes, AsBytes)]
 #[repr(C)]
 pub struct Header64 {
     pub magic: u32,
@@ -203,7 +246,7 @@ pub struct Header64 {
 }
 
 #[repr(C)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, FromBytes, FromZeroes, AsBytes)]
 pub struct Section64 {
     pub sectname: [u8; 16],
     pub segname: [u8; 16],
@@ -221,6 +264,18 @@ pub struct Section64 {
 
 const SEG_LINKEDIT: &[u8] = b"__LINKEDIT";
 const LC_SEGMENT_64: u32 = 0x19;
+const LC_SYMTAB: u32 = 0x2;
+const LC_DYSYMTAB: u32 = 0xb;
+const LC_CODE_SIGNATURE: u32 = 0x1d;
+const LC_FUNCTION_STARTS: u32 = 0x26;
+const LC_DATA_IN_CODE: u32 = 0x29;
+const LC_DYLD_INFO: u32 = 0x22;
+const LC_DYLD_INFO_ONLY: u32 = 0x80000022;
+const LC_DYLIB_CODE_SIGN_DRS: u32 = 0x2b;
+const LC_LINKER_OPTIMIZATION_HINT: u32 = 0x2d;
+const LC_DYLD_EXPORTS_TRIE: u32 = 0x8000001e;
+const LC_DYLD_CHAINED_FIXUPS: u32 = 0x80000034;
+
 fn align(size: u64, base: u64) -> u64 {
     let over = size % base;
     if over == 0 {
@@ -232,6 +287,260 @@ fn align(size: u64, base: u64) -> u64 {
 
 fn align_vmsize(size: u64) -> u64 {
     align(if size > 0x4000 { size } else { 0x4000 }, 0x1000)
+}
+
+fn shift(value: u64, amount: u64, range_min: u64, range_max: u64) -> u64 {
+    if value < range_min || value > (range_max + range_min) {
+        return value;
+    }
+    value + amount
+}
+
+pub struct Macho {
+    header: Header64,
+    commands: Vec<(u32, u32, usize)>,
+    linkedit_cmd: SegmentCommand64,
+    rest_size: u64,
+    data: Vec<u8>,
+    seg: SegmentCommand64,
+    sec: Section64,
+    sectdata: Option<Vec<u8>>,
+}
+
+impl Macho {
+    pub fn from(obj: Vec<u8>) -> Self {
+        let header = Header64::read_from_prefix(&obj).unwrap();
+        let mut commands: Vec<(u32, u32, usize)> = Vec::new();
+
+        let mut offset = std::mem::size_of::<Header64>() as usize;
+        let mut linkedit_cmd = None;
+
+        for _ in 0..header.ncmds as usize {
+            let cmd = u32::from_le_bytes(obj[offset..offset + 4].try_into().unwrap());
+            let cmdsize = u32::from_le_bytes(obj[offset + 4..offset + 8].try_into().unwrap());
+
+            if cmd == LC_SEGMENT_64 {
+                let segcmd = SegmentCommand64::read_from_prefix(&obj[offset..]).unwrap();
+                if segcmd.segname == SEG_LINKEDIT {
+                    linkedit_cmd = Some(segcmd);
+                }
+            }
+
+            commands.push((cmd, cmdsize, offset));
+            offset += cmdsize as usize;
+        }
+
+        let Some(linkedit_cmd) = linkedit_cmd else {
+            panic!("Failed to find __LINKEDIT segment");
+        };
+        let rest_size = linkedit_cmd.fileoff - offset as u64 - header.sizeofcmds as u64;
+        Self {
+            header,
+            commands,
+            linkedit_cmd,
+            data: obj,
+            rest_size,
+            seg: SegmentCommand64::new_zeroed(),
+            sec: Section64::new_zeroed(),
+            sectdata: None,
+        }
+    }
+
+    pub fn write_section(mut self, name: &str, sectdata: Vec<u8>) -> Result<Self, Error> {
+        self.seg.cmd = LC_SEGMENT_64;
+        self.seg.cmdsize = std::mem::size_of::<SegmentCommand64>() as u32
+            + std::mem::size_of::<Section64>() as u32;
+
+        // Copy segment name
+        let segname = name.as_bytes();
+        for i in 0..segname.len() {
+            self.seg.segname[i] = segname[i] as u8;
+        }
+
+        self.seg.vmaddr = self.linkedit_cmd.vmaddr;
+        self.seg.vmsize = align_vmsize(sectdata.len() as u64);
+        self.seg.fileoff = self.linkedit_cmd.fileoff;
+        self.seg.filesize = self.seg.vmsize;
+        self.seg.maxprot = 0x01;
+        self.seg.initprot = self.seg.maxprot;
+        self.seg.nsects = 1;
+
+        // Copy section name
+        let sectname = name.as_bytes();
+        for i in 0..sectname.len() {
+            self.sec.sectname[i] = sectname[i] as u8;
+        }
+        self.sec.segname = *b"__SUI\0\0\0\0\0\0\0\0\0\0\0";
+
+        self.sec.addr = self.seg.vmaddr;
+        self.sec.size = sectdata.len() as u64;
+        self.sec.offset = self.seg.fileoff as u32;
+        self.sec.align = if self.sec.size < 15 { 0 } else { 4 };
+
+        self.linkedit_cmd.vmaddr += self.seg.vmsize;
+        let linkedit_fileoff = self.linkedit_cmd.fileoff;
+        self.linkedit_cmd.fileoff += self.seg.filesize;
+
+        macro_rules! shift_cmd {
+            ($cmd:expr) => {
+                $cmd = shift(
+                    $cmd as _,
+                    self.seg.filesize,
+                    linkedit_fileoff,
+                    self.linkedit_cmd.filesize,
+                ) as _;
+            };
+        }
+        for (cmd, _, offset) in self.commands.iter_mut() {
+            match *cmd {
+                LC_SYMTAB => {
+                    #[derive(FromBytes, FromZeroes, AsBytes)]
+                    #[repr(C)]
+                    pub struct SymtabCommand {
+                        pub cmd: u32,
+                        pub cmdsize: u32,
+                        pub symoff: u32,
+                        pub nsyms: u32,
+                        pub stroff: u32,
+                        pub strsize: u32,
+                    }
+
+                    let cmd = SymtabCommand::mut_from_prefix(&mut self.data[*offset..]).unwrap();
+                    shift_cmd!(cmd.symoff);
+                    shift_cmd!(cmd.stroff);
+                }
+                LC_DYSYMTAB => {
+                    #[derive(FromBytes, FromZeroes, AsBytes)]
+                    #[repr(C)]
+                    pub struct DysymtabCommand {
+                        pub cmd: u32,
+                        pub cmdsize: u32,
+                        pub ilocalsym: u32,
+                        pub nlocalsym: u32,
+                        pub iextdefsym: u32,
+                        pub nextdefsym: u32,
+                        pub iundefsym: u32,
+                        pub nundefsym: u32,
+                        pub tocoff: u32,
+                        pub ntoc: u32,
+                        pub modtaboff: u32,
+                        pub nmodtab: u32,
+                        pub extrefsymoff: u32,
+                        pub nextrefsyms: u32,
+                        pub indirectsymoff: u32,
+                        pub nindirectsyms: u32,
+                        pub extreloff: u32,
+                        pub nextrel: u32,
+                        pub locreloff: u32,
+                        pub nlocrel: u32,
+                    }
+
+                    let cmd = DysymtabCommand::mut_from_prefix(&mut self.data[*offset..]).unwrap();
+                    shift_cmd!(cmd.tocoff);
+                    shift_cmd!(cmd.modtaboff);
+                    shift_cmd!(cmd.extrefsymoff);
+                    shift_cmd!(cmd.indirectsymoff);
+                    shift_cmd!(cmd.extreloff);
+                    shift_cmd!(cmd.locreloff);
+                }
+
+                LC_CODE_SIGNATURE
+                | LC_FUNCTION_STARTS
+                | LC_DATA_IN_CODE
+                | LC_DYLIB_CODE_SIGN_DRS
+                | LC_LINKER_OPTIMIZATION_HINT
+                | LC_DYLD_EXPORTS_TRIE
+                | LC_DYLD_CHAINED_FIXUPS => {
+                    #[derive(FromBytes, FromZeroes, AsBytes)]
+                    #[repr(C)]
+                    struct LinkeditDataCommand {
+                        cmd: u32,
+                        cmdsize: u32,
+                        dataoff: u32,
+                        datasize: u32,
+                    }
+                    let cmd =
+                        LinkeditDataCommand::mut_from_prefix(&mut self.data[*offset..]).unwrap();
+                    shift_cmd!(cmd.dataoff);
+                }
+
+                LC_DYLD_INFO | LC_DYLD_INFO_ONLY => {
+                    #[derive(FromBytes, FromZeroes, AsBytes)]
+                    #[repr(C)]
+                    pub struct DyldInfoCommand {
+                        pub cmd: u32,
+                        pub cmdsize: u32,
+                        pub rebase_off: u32,
+                        pub rebase_size: u32,
+                        pub bind_off: u32,
+                        pub bind_size: u32,
+                        pub weak_bind_off: u32,
+                        pub weak_bind_size: u32,
+                        pub lazy_bind_off: u32,
+                        pub lazy_bind_size: u32,
+                        pub export_off: u32,
+                        pub export_size: u32,
+                    }
+                    let dyld_info =
+                        DyldInfoCommand::mut_from_prefix(&mut self.data[*offset..]).unwrap();
+                    shift_cmd!(dyld_info.rebase_off);
+                    shift_cmd!(dyld_info.bind_off);
+                    shift_cmd!(dyld_info.weak_bind_off);
+                    shift_cmd!(dyld_info.lazy_bind_off);
+                    shift_cmd!(dyld_info.export_off);
+                }
+
+                _ => {}
+            }
+        }
+
+        self.header.ncmds += 1;
+        self.header.sizeofcmds += self.seg.cmdsize;
+
+        self.sectdata = Some(sectdata);
+        Ok(self)
+    }
+
+    /// Build and write the modified Mach-O file
+    pub fn build<W: std::io::Write + std::io::Seek>(mut self, writer: &mut W) -> Result<(), Error> {
+        writer.write(self.header.as_bytes())?;
+
+        let mut off = std::mem::size_of::<Header64>();
+        for (cmd, cmdsize, offset) in self.commands.iter_mut() {
+            if *cmd == LC_SEGMENT_64 {
+                let segcmd = SegmentCommand64::read_from_prefix(&self.data[*offset..]).unwrap();
+                if segcmd.segname == SEG_LINKEDIT {
+                    writer.write(self.seg.as_bytes())?;
+                    writer.write(self.sec.as_bytes())?;
+                }
+            }
+            writer.write_all(&self.data[*offset..*offset + *cmdsize as usize])?;
+            off += *cmdsize as usize;
+        }
+
+        // Write rest of the data
+        let len = self.rest_size as usize - self.seg.cmdsize as usize;
+        writer.write(&self.data[off..off + len])?;
+
+        // Write custom data
+        if let Some(sectdata) = self.sectdata {
+            writer.write(&sectdata)?;
+            if self.seg.filesize > sectdata.len() as u64 {
+                let padding = vec![0; (self.seg.filesize - sectdata.len() as u64) as usize];
+                writer.write(&padding)?;
+            }
+        }
+
+        // Write __LINKEDIT data
+        writer.write(&self.data[off + len..off + len + self.linkedit_cmd.filesize as usize])?;
+
+        println!(
+            "Total size: {}",
+            off + len + self.linkedit_cmd.filesize as usize
+        );
+
+        Ok(())
+    }
 }
 
 pub fn inject_macho(
