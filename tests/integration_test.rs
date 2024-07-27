684636466344745729658cd5adb1019d1e17926c
Divy Srivastava <dj.srivastava23@gmail.com>
2024-07-27 10:39:55 +0530

    x

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,7 @@ use libsui::{Elf, Macho, PortableExecutable};
 
 const RESOURCE_NAME: &str = "sui_test_data";
 
-macro_rules! binary_tests {
+macro_rules! data_size_tests {
     ($test_fn:ident, $($name:ident: $value:expr),*) => {
     $(
         #[test]
@@ -13,19 +13,41 @@ macro_rules! binary_tests {
     }
 }
 
+#[cfg(unix)]
+use std::os::unix::fs::OpenOptionsExt;
+
 fn test_macho(size: usize) {
     let input = std::fs::read("tests/exec_mach64").unwrap();
     let mut macho = Macho::from(input).unwrap();
 
     let data = vec![0; size];
+    #[cfg(not(target_os = "macos"))]
+    let mut out = std::io::sink();
+    #[cfg(target_os = "macos")]
+    let mut out = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .mode(0o755)
+        .open(&std::env::temp_dir().join("exec_mach64_out"))
+        .unwrap();
     macho
         .write_section(RESOURCE_NAME, data)
         .unwrap()
-        .build_and_sign(std::io::sink())
+        .build_and_sign(&mut out)
         .unwrap();
+
+    #[cfg(target_os = "macos")]
+    {
+        drop(out);
+        // Run the output
+        let output = std::process::Command::new("tests/exec_mach64_out")
+            .output()
+            .unwrap();
+        assert_eq!(output.status.success(), true);
+    }
 }
 
-binary_tests! {
+data_size_tests! {
     test_macho,
     test_macho_0: 0,
     test_macho_1: 1,
@@ -42,23 +64,20 @@ fn test_elf(size: usize) {
     let elf = Elf::new(&input);
 
     let data = vec![0; size];
+    #[cfg(not(target_os = "linux"))]
     let mut out = std::io::sink();
+    #[cfg(target_os = "linux")]
+    let mut out = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .mode(0o755)
+        .open(&std::env::temp_dir().join("exec_elf64_out"))
+        .unwrap();
+
     elf.append(&data, &mut out).unwrap();
 
-    #[cfg(target_os = "linux")]
+    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
     {
-        use std::os::unix::fs::OpenOptionsExt;
-
-        let out = std::env::temp_dir().join("exec_elf64_out");
-        // Write the output to a file
-        let mut out = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .mode(0o755)
-            .open(&out)
-            .unwrap();
-        elf.append(&data, &mut out).unwrap();
-
         drop(out);
         // Run the output
         let output = std::process::Command::new("tests/exec_elf64_out")
@@ -68,7 +87,7 @@ fn test_elf(size: usize) {
     }
 }
 
-binary_tests! {
+data_size_tests! {
     test_elf,
     test_elf_0: 0,
     test_elf_1: 1,
@@ -80,5 +99,26 @@ binary_tests! {
     test_elf_1024_1024_5 : 1024 * 1024 * 5
 }
 
-#[test]
-fn test_pe() {}
+fn test_pe(size: usize) {
+    let input = std::fs::read("tests/exec_pe64").unwrap();
+    let mut pe = PortableExecutable::from(&input).unwrap();
+
+    let data = vec![0; size];
+    let mut out = std::io::sink();
+    pe.write_resource(RESOURCE_NAME, data)
+        .unwrap()
+        .build(&mut out)
+        .unwrap();
+}
+
+data_size_tests! {
+    test_pe,
+    test_pe_0: 0,
+    test_pe_1: 1,
+    test_pe_10: 10,
+    test_pe_64: 64,
+    test_pe_512: 512,
+    test_pe_1024: 1024,
+    test_pe_1024_1024: 1024 * 1024,
+    test_pe_1024_1024_5 : 1024 * 1024 * 5
+}
